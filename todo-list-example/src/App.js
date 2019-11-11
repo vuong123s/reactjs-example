11565060a529513e66ed829310d9d5c64e4589fb
Quốc Trung <43474427+miyucoder@users.noreply.github.com>
2019-11-11 18:30:13 +0700

    8-render-a-list

--- todo-list-example/src/App.js
+++ todo-list-example/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.TodoItems = [
+            'Mua bim bim',
+            'Đi đá bóng',
+            'Đi đổ xăng'
+        ];
+    }
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.TodoItems.map((item, index) => {
+                        return <TodoItem key={index} title={item} />
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
